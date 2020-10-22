952e27bbb5cb147dbcc022754d18336185a4efaf
Esteban <hestebanramos@gmail.com>
2020-10-21 21:48:28 -0400

    Localisation de départ par Google Map

--- app/src/main/java/ca/qc/bdeb/p55/tp1/MapsActivity.java
+++ app/src/main/java/ca/qc/bdeb/p55/tp1/MapsActivity.java
@@ -1,24 +1,45 @@
 package ca.qc.bdeb.p55.tp1;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
+    private static final String TAG = "ca.qc.bdeb.p55.tp1";
 
+    private static final int LOCATION_REQUEST_CODE = 1;
     private GoogleMap mMap;
+    private FusedLocationProviderClient fusedLocationClient;
+
+    private double latitude = 0;
+    private double longitude = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -38,9 +59,76 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //Demande de permissions:
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_REQUEST_CODE);
+        } else {
+            activeLocalisation();
+        }
+
+
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private void activeLocalisation() {
+        if (mMap != null) {
+            mMap.setMyLocationEnabled(true);
+            fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+                        Log.i(TAG, "Ma localisation " + location);
+                        addMarkerToPosition();
+                    }
+                }
+            });
+        }
+    }
+
+    private void addMarkerToPosition() {
+        LatLng pos = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(pos).title("Position Coureur").snippet("Départ"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(pos, 16));
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if (isPermissionAuth(permissions, grantResults, Manifest.permission.ACCESS_FINE_LOCATION) ||
+                    isPermissionAuth(permissions, grantResults, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                activeLocalisation();
+            }
+        }
+    }
+
+    private boolean isPermissionAuth(String[] permissions, int[] grantResults, String permission) {
+        for (int i = 0; i < permissions.length; i++) {
+            if (permissions[i].compareToIgnoreCase(permission) == 0) {
+                return (grantResults[i] == PackageManager.PERMISSION_GRANTED);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "onMyLocationButtonClick", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "onMyLocationClick \n" + location, Toast.LENGTH_SHORT).show();
+
     }
 }
